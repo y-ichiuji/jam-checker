2f46c2af97e07bb7efb991b89fb12d3abb2d47bc
y-ichiuji <y-ichiuji@safie.jp>
2025-05-09 08:15:02 +0900

    remove: 不要なファイルを削除

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,16 +15,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'jam-checker' title`, () => {
+  it(`should have the 'JamChecker' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jam-checker');
+    expect(app.title).toEqual('JamChecker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, jam-checker');
+    expect(compiled.querySelector('h1')?.textContent).toContain('JamChecker');
   });
 });
